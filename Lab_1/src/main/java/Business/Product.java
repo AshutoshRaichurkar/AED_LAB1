e7d7178fcc5a20b9532b4ec011e370bdd91f5444
AshutoshRaichurkar <55558334+AshutoshRaichurkar@users.noreply.github.com>
2019-09-21 22:11:22 -0400

    Add files via upload

---
+++ Lab_1/src/main/java/Business/Product.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Raichurkar
+ */
+public class Product {
+    private String name;
+    private String price;
+    private String availNum;
+    private String disc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getDisc() {
+        return disc;
+    }
+
+    public void setDisc(String disc) {
+        this.disc = disc;
+    }
+
+   
+    
+    
+}
